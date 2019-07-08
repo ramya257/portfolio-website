4b23a57549b1fe6827606fdd4646aa495ebd9ba5
Meghal Agrawal <educate1996@gmail.com>
2019-07-08 23:37:46 +0530

    Updates

---
+++ precache-manifest.91720596dbc43e3d4f5a6e879af6d21e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c584fe1e11a9d2a33c9f3d0afb5fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0370920d046740ce62f",
+    "url": "/static/css/main.d549e8f1.chunk.css"
+  },
+  {
+    "revision": "f5470452aec0bd61a447",
+    "url": "/static/js/2.4cbfd1a3.chunk.js"
+  },
+  {
+    "revision": "d0370920d046740ce62f",
+    "url": "/static/js/main.13586210.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
