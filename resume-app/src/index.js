b0ec78021b533f2e4f45fdace3e36483761bb094
Meghal Agrawal <educate1996@gmail.com>
2019-07-08 23:19:13 +0530

    started landing page

--- resume-app/src/index.js
+++ resume-app/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const divStyle={
+width:'50rem',
+height:'30rem',
+align:'center',
+display:'inline-block'
+};
+
+class App extends React.Component{
+
+
+render (){
+    return (
+        <div className="App">
+        <div className="card card bg-light mb-3" style={divStyle}>
+        <div className="card-header">Hi ! I am V.Ramya</div>
+        <div className="card-body">
+          <h5 className="card-title">Info card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
