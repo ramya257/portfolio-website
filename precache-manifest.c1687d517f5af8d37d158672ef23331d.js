2260d53e724a2984d4efa83213bccaebc44aeda6
Meghal Agrawal <educate1996@gmail.com>
2019-07-25 22:40:57 +0530

    Updates

---
+++ precache-manifest.c1687d517f5af8d37d158672ef23331d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6405006e9401192ef81e0bc2ef582eaa",
+    "url": "/ramya257.github.io/index.html"
+  },
+  {
+    "revision": "d0370920d046740ce62f",
+    "url": "/ramya257.github.io/static/css/main.d549e8f1.chunk.css"
+  },
+  {
+    "revision": "f5470452aec0bd61a447",
+    "url": "/ramya257.github.io/static/js/2.4cbfd1a3.chunk.js"
+  },
+  {
+    "revision": "d0370920d046740ce62f",
+    "url": "/ramya257.github.io/static/js/main.13586210.chunk.js"
+  },
+  {
+    "revision": "2f65f66dd7e700d2f254",
+    "url": "/ramya257.github.io/static/js/runtime~main.406d65d1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mywebsite/precache-manifest.d93e25b5efd5c817817223bd8809399c.js"
+  "/ramya257.github.io/precache-manifest.c1687d517f5af8d37d158672ef23331d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mywebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ramya257.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
