7beab8badd5c279b94a1a27916a002f46e369525
Meghal Agrawal <educate1996@gmail.com>
2019-07-26 23:25:09 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Welcome to My Website
-      </header>
-
-
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/Home">Navbar</a>
+      <nav className="navbar sticky-top navbar-custom">
+  
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link"href="/Home" >Home <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link"href="/Home">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/Home">Link</a>
+      <li className="nav-item">
+        <a className="nav-link" href="/About">About</a>
       </li>
-    
+    <li className="nav-item">
+    <a className="nav-link" href="/Skills">Skills</a>
+    </li>
     
     </ul>
   
