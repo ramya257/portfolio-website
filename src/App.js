4a4184f2df1ebf9b2d3b1ea10ebaf85df25f3c19
Meghal Agrawal <educate1996@gmail.com>
2019-07-26 23:01:51 +0530

    added bootstrap and nav bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,32 @@ function App() {
       <header className="App-header">
        Welcome to My Website
       </header>
-    </div>
+
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="/Home">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link"href="/Home" >Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/Home">Link</a>
+      </li>
+    
+    
+    </ul>
+  
+  </div>
+  </nav>
+ 
+
+      </div>
+
   );
 }
 
